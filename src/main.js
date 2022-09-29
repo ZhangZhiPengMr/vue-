1d18282349a5eb8ce08b93cb4635cbf556bb1e5c
ZhangZhiPengMr <3067673423@qq.com>
2022-09-29 10:00:07 +0800

    feat(custom): 安装了项目的所需依赖

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入elementUI组件样式
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 //引入css初始化样式
 import "./style/resate.css"
+//挂载elementUI
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
