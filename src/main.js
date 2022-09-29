c358db4ad19dc99c1add1d0bb21690cc48dfa29a
ZhangZhiPengMr <3067673423@qq.com>
2022-09-29 09:52:57 +0800

    feat(custom): 对项目进行基础配置

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+//引入css初始化样式
+import "./style/resate.css"
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    //项目的端口号
+    port: 8090,
+    //主机名称
+    host: "localhost",
+    // 是否开启https
+    https:false,
+    //是否自动打开浏览器
+    open:true,
+  }
 })
